001a52d3bd221a3bf1e7e8f0290f9bae10b902ef
Austin Godber <godber@uberhip.com>
2013-04-11 21:32:40 -0700

    tweaked

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ AUTHOR = u'DesertPy Pythonistas'
 SITENAME = u'DesertPy'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/Phoenix'
 
 DEFAULT_LANG = u'en'
 
@@ -16,7 +16,8 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('@desertpy', 'https://twitter.com/desertpy'),
+          ('DesertPy Meetup', 'http://www.meetup.com/Phoenix-Python-Meetup-Group'),)
 
 DEFAULT_PAGINATION = 10
+DISPLAY_PAGES_ON_MENU = True
