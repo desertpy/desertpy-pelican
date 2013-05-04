052191549e8d2a1ee4c5323a18edd7acec06c34f
Austin Godber <godber@uberhip.com>
2013-05-04 08:20:32 -0700

    updated template

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,13 +11,15 @@ DEFAULT_LANG = u'en'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+          ('Python.org', 'http://python.org'))
 
 # Social widget
-SOCIAL = (('@desertpy', 'https://twitter.com/desertpy'),
-          ('DesertPy Meetup', 'http://www.meetup.com/Phoenix-Python-Meetup-Group'),)
+SOCIAL = (('Twitter', 'https://twitter.com/desertpy'),
+          ('Github', 'https://github.com/desertpy'),
+          ('Google+', 'https://plus.google.com/communities/103511724147602323431'),
+          ('Meetup', 'http://www.meetup.com/Phoenix-Python-Meetup-Group'),)
 
 DEFAULT_PAGINATION = 10
 DISPLAY_PAGES_ON_MENU = True
+
+THEME = "themes/subtle"
